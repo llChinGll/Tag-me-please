51d2e6cb2a4ee09bdda50ce340640b0b7f64cc3a
Jeab <dadajang19107@gmail.com>
2014-02-22 15:54:14 +0700

    insert code for train.py

--- training.py
+++ training.py
@@ -1,7 +1,22 @@
 def train(data,dic):
-	print " == train =="
+    #data = ['a','a','a','f','g','h','i','i']
+    #dic = {'a':1,'b':2,'c':3,'d':4,'e':5}
+    KB = {}
+    for i in data :
+        if dic.has_key(i):
+	#update +1
+	    val = dic[i]
+	    dic[i] = val+1
+	else:
+	    dic[i] = 1
+	#add key val = 1
+    print " == train =="
 	#some code
 	#some code
 	#return KB
+    KB = dic
+    for key in KB:
+	print key + "=" + str(KB[key])
+    return KB
 	
 
